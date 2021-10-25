6505051a2f6c1a80a22178c34c0e0052e97bc048
Takács Balázs <tbalazs.levente@gmail.com>
2021-10-25 18:23:28 +0200

    Szamla osztály

---
+++ src/com/company/bankiszolgaltatasok/Szamla.java
@@ -0,0 +1,20 @@
+package com.company.bankiszolgaltatasok;
+
+public abstract class Szamla extends BankiSzolgaltatas {
+    private int aktualisEgyenleg;
+
+
+    public Szamla(Tulajdonos tulajdonos) {
+        super(tulajdonos);
+    }
+
+    public int getAktualisEgyenleg() {
+        return aktualisEgyenleg;
+    }
+
+    public void befizet(int osszeg) {
+        this.aktualisEgyenleg += osszeg;
+    }
+
+    public abstract boolean kivesz(int osszeg);
+}
